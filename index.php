11d364b7c2a5ca1b89226365c6e65581d1b52d5f
roshni5610 <84753022+roshni5610@users.noreply.github.com>
2021-06-09 16:05:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> insert operation in php </title>
+</head>
+<body>
+<form action="insertdata.php" method="post">
+<table >
+<tr>
+ <td>name:</td> <td> <input type="text" name="name" required="required"/>  </td>
+ </tr>
+ <tr> 
+ <td> email id:</td> <td> <input type="text" name="email" required="required"/> </td>
+ </tr>
+ <tr>
+ <td> <input type="submit" name="submit" value="submit"/> </td>
+ </tr>
+</table>
+</body>
+
+</html>
