11214898996b7acd898743ee770952ac042c42dc
roshni5610 <84753022+roshni5610@users.noreply.github.com>
2021-06-09 15:55:58 +0530

    Add files via upload

---
+++ day5/Associativearray.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h3> Associative array </h3> ";
+$c=array(0=> 10,"c"=>"computer","php"=>"web development",10=>"ten",50=>50.5);
+
+foreach($c as $value)
+{
+	echo "<br> value is $value";
+}
+
+
+?>
